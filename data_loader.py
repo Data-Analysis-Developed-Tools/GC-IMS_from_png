c98bf58540212e8afd0aca1322859444c45d7259
Franco Rosso <169049117+FrancoR72@users.noreply.github.com>
2025-02-15 17:43:37 +0100

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -2,8 +2,8 @@ import streamlit as st
 from PIL import Image
 
 def load_image():
-    """Carica un'immagine PNG e restituisce un oggetto PIL.Image."""
-    uploaded_file = st.file_uploader("Carica un'immagine (.png)", type=["png"])
+    """Carica un'immagine PNG dalla barra laterale e restituisce un oggetto PIL.Image."""
+    uploaded_file = st.sidebar.file_uploader("📂 Carica un'immagine (.png)", type=["png"])
     
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
